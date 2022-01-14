3be7fd622f9668bca93993d93567ff95cc9c0e4f
rased100 <rased100@gmail.com>
2022-01-14 23:14:00 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is header</h1>
+            {/* <NavLink></NavLink> */}
+        </div>
+    );
+};
+
+export default Header;
